e8269dc3842d53c4f540eabe6514c6d82e9f2293
Juliano C L Santos <foxports@hotmail.com>
2025-05-30 01:23:28 -0300

    first commit

---
+++ src/main/java/com/julianoclsantos/wallet_assignment_service/WalletAssignmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.julianoclsantos.wallet_assignment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletAssignmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletAssignmentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julianoclsantos/wallet_assignment_service/WalletAssignmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianoclsantos.wallet_assignment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletAssignmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
